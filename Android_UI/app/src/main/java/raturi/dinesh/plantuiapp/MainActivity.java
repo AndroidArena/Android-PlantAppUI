0c147223aa6841a33452bb8d27216888e8315d84
Dinesh Raturi <dineshraturi22@gmail.com>
2021-03-06 20:25:32 +0530

    Add files via upload

---
+++ Android_UI/app/src/main/java/raturi/dinesh/plantuiapp/MainActivity.java
@@ -0,0 +1,18 @@
+package raturi.dinesh.plantuiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
